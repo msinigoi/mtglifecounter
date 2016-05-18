69e631587512eacac2bc58d9ccf4ffec581c72ae
Michele Sinigoi <Maik@SlurmMcKenzie.local>
2016-05-18 21:02:24 +0200

    +5 -5 buttons now add and subtract lifetotals

--- js/lifecounter.js
+++ js/lifecounter.js
@@ -12,6 +12,20 @@ $("#plusButton1P1").click(function(){
   $("#player1LifeTotal").text(lifeTotalAsInt);
 });
 
+$("#minusButton5P1").click(function(){
+  var lifeTotal = ($("#player1LifeTotal").text());
+  var lifeTotalAsInt = parseInt(lifeTotal, 10);
+  var lifeTotalAsInt = lifeTotalAsInt-5;
+  $("#player1LifeTotal").text(lifeTotalAsInt);
+});
+
+$("#plusButton5P1").click(function(){
+  var lifeTotal = ($("#player1LifeTotal").text());
+  var lifeTotalAsInt = parseInt(lifeTotal, 10);
+  var lifeTotalAsInt = lifeTotalAsInt+5;
+  $("#player1LifeTotal").text(lifeTotalAsInt);
+});
+
 $("#minusButton1P2").click(function(){
   var lifeTotal = ($("#player2LifeTotal").text());
   var lifeTotalAsInt = parseInt(lifeTotal, 10);
@@ -23,5 +37,19 @@ $("#plusButton1P2").click(function(){
   var lifeTotal = ($("#player2LifeTotal").text());
   var lifeTotalAsInt = parseInt(lifeTotal, 10);
   var lifeTotalAsInt = lifeTotalAsInt+1;
+  $("#player2LifeTotal").text(lifeTotalAsInt);
+});
+
+$("#minusButton5P2").click(function(){
+  var lifeTotal = ($("#player2LifeTotal").text());
+  var lifeTotalAsInt = parseInt(lifeTotal, 10);
+  var lifeTotalAsInt = lifeTotalAsInt-5;
+  $("#player2LifeTotal").text(lifeTotalAsInt);
+});
+
+$("#plusButton5P2").click(function(){
+  var lifeTotal = ($("#player2LifeTotal").text());
+  var lifeTotalAsInt = parseInt(lifeTotal, 10);
+  var lifeTotalAsInt = lifeTotalAsInt+5;
   $("#player2LifeTotal").text(lifeTotalAsInt);
 });
